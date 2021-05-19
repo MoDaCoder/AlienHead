e4aed4a86a8a1edf7beb0e23ce3be63650938181
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-19 16:30:26 -0500

    Creating a window screen 'Let's Build a Game!'

--- FirstJavaGame/src/firstjavagame/Game.java
+++ FirstJavaGame/src/firstjavagame/Game.java
@@ -1,7 +1,28 @@
 package firstjavagame;
 
-public class Game {
-    public static void main(String[] args){
-        
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
+
+    @SuppressWarnings("compatibility:-8218418520252624046")
+    private static final long serialVersionUID = 1L;
+
+    public static final int WIDTH = 2400, HEIGHT = WIDTH / 12*9; /*Formula for frame size*/
+    
+
+    public Game(){ /*Game constructor*/
+        new Window(WIDTH, HEIGHT, "Let's Build  a Game!", this);
+    }
+
+    public synchronized void start(){
+    
+    }
+
+    public void run(){
+
+    }
+
+    public static void main(String[] args) {
+        new Game();
     }
 }
--- FirstJavaGame/src/firstjavagame/Window.java
+++ FirstJavaGame/src/firstjavagame/Window.java
@@ -11,19 +11,20 @@ public class Window extends Canvas{
     @SuppressWarnings("compatibility:7629012207966341723")
     private static final long serialVersionUID = 1L;
     
-    public Window(int width, int height, String title, Game game){
+    public Window(int width, int height, String title, Game game /*Instance of a game in Window's construction parameters*/){
         JFrame frame = new JFrame(title);
+        /*Lines 17-27 are setting up the JFrame*/
         
+        /*Lines 19-21 setting the window size*/
         frame.setPreferredSize(new Dimension(width, height));
         frame.setMaximumSize(new Dimension(width, height));
         frame.setMinimumSize(new Dimension(width, height));
         
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.add(game);
-        frame.setVisible(true);
-        frame.setVisible(true);
-        game.start(); /*This is a function inside of Game Class*/
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); /*Allows us to actually stop the thread in the games operations*/
+        frame.setResizable(false); /*Allowing the ability to re-size window to false*/
+        frame.setLocationRelativeTo(null); /*Sets window to start in the middle of the screen*/
+        frame.add(game); /*Adding Game class to frame*/
+        frame.setVisible(true); /*Setting frame to visible so we can see it*/
+        game.start(); /*Running the start method on Game class*/
     }
 }
