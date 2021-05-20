6a5b0f27be4b22fe1fed13750a2ac95c3a4bcb22
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-20 11:59:33 -0500

    Creating GameObject Class so it can pass down functions to all objects in this game.

---
+++ FirstJavaGame/src/firstjavagame/GameObject.java
@@ -0,0 +1,5 @@
+package firstjavagame;
+                                    /*GameObject class acts as an umbrella for every single object in this game*//*Every Object in this game will be considered a GameObject*/
+public abstract class GameObject {  /*All of the objects in this game will inherit all of the functions from GameObject*/
+        
+}
